f6e926bc2f3f44161423973a24fd6b66bacded80
vish <vishdeadpool@gmail.com>
2024-09-05 13:40:54 +0545

    Update task-4-starter.cpp

--- task-4-starter.cpp
+++ task-4-starter.cpp
@@ -95,10 +95,10 @@ public:
 
     void remove_item(int item_index) {
         int input_quantity;
-        Item *item = items[item_index];
+        Item* item = items[item_index];
         std::cout << "\nEnter number of items to sell: ";
         std::cin >> input_quantity;
-
+    
         int quantity = item->get_quantity();
         if (input_quantity <= quantity) {
             float price = item->get_price();
@@ -107,10 +107,23 @@ public:
             std::cout << "\nItems sold";
             std::cout << "\nMoney received: " << money_earned;
             total_money += money_earned;
+    
+            // Remove the item if quantity is zero
+            if (item->get_quantity() == 0) {
+                delete item; // Free the memory of the item
+    
+                // Shift items to remove the gap
+                for (int i = item_index; i < item_count - 1; i++) {
+                    items[i] = items[i + 1];
+                }
+                items[item_count - 1] = nullptr; // Set the last item to nullptr
+                item_count--; // Decrease the item count
+            }
         } else {
             std::cout << "\nCannot sell more items than you have.";
         }
     }
+
 
     void list_items() {
         if (item_count == 0) {
